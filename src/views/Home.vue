0fb71b5f5c52d25bdbd2cd9da386a2aa58a9385d
xiehaopei <15907678645@163.com>
2020-11-02 23:59:50 +0800

    添加icon图标

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,35 +8,35 @@
         <div class="nav">
           <ul>
             <li>
-              <router-link to="home">
+              <router-link to="welcome">
                 <svg class="icon" aria-hidden="true">
                   <use xlink:href="#icon-home" />
                 </svg>Home
               </router-link>
             </li>
             <li>
-              <router-link to="home/archives">
+              <router-link to="archives">
                 <svg class="icon" aria-hidden="true">
                   <use xlink:href="#icon-archives" />
                 </svg>Archives
               </router-link>
             </li>
             <li>
-              <router-link to="home/guestbook">
+              <router-link to="guestbook">
                 <svg class="icon" aria-hidden="true">
-                  <use xlink:href="#icon-comments" />
+                  <use xlink:href="#icon-comment" />
                 </svg>Guestbook
               </router-link>
             </li>
             <li>
-              <router-link to="home/links">
+              <router-link to="links">
                 <svg class="icon" aria-hidden="true">
                   <use xlink:href="#icon-link" />
                 </svg>Links
               </router-link>
             </li>
             <li>
-              <router-link to="home/about">
+              <router-link to="about">
                 <svg class="icon" aria-hidden="true">
                   <use xlink:href="#icon-personal" />
                 </svg>About
@@ -48,10 +48,28 @@
     </div>
 
     <div class="middle">
-      middle
       <router-view></router-view>
     </div>
-    <div class="right">right</div>
+    <div class="right">
+      <div class="archives right-item">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-time" />
+        </svg>
+        <font>ARCHIVES</font>
+      </div>
+      <div class="tags right-item">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-tag" />
+        </svg>
+        <font>TAGS</font>
+      </div>
+      <div class="comments right-item">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-comments" />
+        </svg>
+        <font>CURRENCY COMMENTS</font>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -68,7 +86,8 @@ export default {};
 
   .left {
     padding: 30px 10px;
-    width: 280px;
+    width: 240px;
+    min-width: 220px;
 
     > .aside {
       display: flex;
@@ -76,10 +95,10 @@ export default {};
       align-items: center;
 
       > img {
+        margin-bottom: 20px;
+        border-radius: 50%;
         width: 200px;
         height: 200px;
-        border-radius: 50%;
-        margin-bottom: 20px;
       }
 
       > h1 {
@@ -87,19 +106,32 @@ export default {};
       }
 
       > h3 {
-        color: #6b6874;
         margin-bottom: 20px;
+        color: #6b6874;
       }
 
       .nav {
-        > li {
-          > a {
-            display: block;
-            padding: 4px 16px;
-            text-decoration: none;
-          }
-          > .router-link-active {
-            background-color: #fff;
+        > ul {
+          > li {
+            margin: 10px;
+            font-size: 20px;
+
+            > a {
+              display: inline-block;
+              padding: 4px 16px;
+              text-decoration: none;
+              color: #d4d4d4;
+
+              .icon {
+                margin-right: 20px;
+                width: 20px;
+                height: 20px;
+              }
+            }
+
+            > .router-link-active {
+              color: #000;
+            }
           }
         }
       }
@@ -109,19 +141,27 @@ export default {};
   .middle {
     flex: 1;
     padding: 1rem 2rem;
+    height: 100vh;
     background-color: #66f;
     font-size: 2rem;
   }
 
   .right {
     padding: 30px 10px;
-    width: 200px;
-    background-color: #3c9;
+    width: 240px;
+
+    .right-item {
+      margin-bottom: 20px;
+
+      font {
+        color: #6b6874;
+      }
+    }
   }
 }
 
 // phone
-@media screen and (max-width: 760px) {
+@media screen and (max-width: 768px) {
   .left {
     display: none;
   }
@@ -132,7 +172,7 @@ export default {};
 }
 
 // iPad
-@media screen and (min-width: 768px) and (max-width: 1023px) {
+@media screen and (min-width: 768px) and (max-width: 1024px) {
   .right {
     display: none;
   }
